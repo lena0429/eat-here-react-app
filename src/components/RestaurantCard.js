e598d31d5ba373d3231c0cb45fd913f5a4e88296
Lena Hou <lenahou2013@gmail.com>
2022-01-10 12:06:22 -0500

    make fecth request and mount restaurants

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-function RestaurantCard(props){
+function RestaurantCards(props){
+
+    const restaurantsJSX = props.restaurants.map((restaurant, id) => 
+    <div key={id} className="restaurant-card">
+      <img src={restaurant.image} alt={restaurant.name} />
+      <h4>{restaurant.name}</h4>
+      <p>{restaurant.country}</p>
+    </div>
+    )
+    
     return(
-           <div key={restaurant.id} className="restaurant-card">
-            <img src={restaurant.image} alt={restaurant.name} />
-            <p>{restaurant.name}, {restaurant.country}</p>
-            </div>
+        restaurantsJSX
     )
 }
 
-export default RestaurantCard
+export default RestaurantCards
--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import NewRestaurantForm from '../components/NewRestaurantForm';
+import Loading from '../components/Loading';
+import { fetchRestaurants  } from '../actions/restaurantActions';
 import { Container } from 'react-bootstrap';
+import RestaurantCards from '../components/RestaurantCard';
+
 
 function RestaurantsContainer() {
+    const restaurants = useSelector(state => state.restaurants)
+    const loading = useSelector(state => state.loading)
+    const dispatch = useDispatch()
+
+
+    useEffect(() => {
+        console.log("mounting ")
+         dispatch(fetchRestaurants())
+
+        // cleanup function
+        return () => {
+            console.log(" unmounting restaurants")
+        } 
+    }, [dispatch])
+
   return(
       <Container className="restaurants-container">
-          <h1>Restaurants Container</h1>
           <NewRestaurantForm />  
+          { loading ? <Loading /> : <RestaurantCards restaurants={restaurants} />}
       </Container>
   )
 }
