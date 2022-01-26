290f96e3879b9c758ace2f2d667614583f98db36
Lena Hou <lenahou2013@gmail.com>
2022-01-26 14:24:38 -0500

    fixing bug

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -6,11 +6,11 @@ function MyNavbar(){
   <Navbar bg="dark" variant="dark">
     <Container>
     <Nav className="me-auto">
-      <Nav.Link href="/" className="nav-font">HOME</Nav.Link>
-      <Nav.Link href="/restaurants" className="nav-font">RESTAURANTS</Nav.Link>
-      <Nav.Link href="/restaurants/new" className="nav-font">ADD RESTAURANT</Nav.Link>
-      <Nav.Link href="/reviews" className="nav-font">REVIEWS</Nav.Link>
-      <Nav.Link href="/reviews/new" className="nav-font">NEW REVIEW</Nav.Link>
+      <Link to="/" className="nav-font">HOME</Link>
+      <Link to="/restaurants" className="nav-font">RESTAURANTS</Link>
+      <Link to="/restaurants/new" className="nav-font">ADD RESTAURANT</Link>
+      <Link to="/reviews" className="nav-font">REVIEWS</Link>
+      <Link to="/reviews/new" className="nav-font">NEW REVIEW</Link>
     </Nav>
     </Container>
   </Navbar>
