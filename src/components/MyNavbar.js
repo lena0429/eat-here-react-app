b4c1e93af322dd871a8654ce42859325d730b6d2
Lena Hou <lenahou2013@gmail.com>
2022-01-26 14:36:01 -0500

    adjusting nav link style

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -7,7 +7,7 @@ function MyNavbar(){
   <Navbar bg="dark" variant="dark">
     <Container>
     <Nav className="me-auto">
-      <Link to="/" className="nav-font">HOME</Link>
+      <Link to="/" className="nav-link">HOME</Link>
       <Link to="/restaurants" className="nav-font">RESTAURANTS</Link>
       <Link to="/restaurants/new" className="nav-font">ADD RESTAURANT</Link>
       <Link to="/reviews" className="nav-font">REVIEWS</Link>
